1653f6e2c18a179fd7edff9b7e2692d4269b9aeb
Korn Timaroon <86138908+korntimaroon@users.noreply.github.com>
2022-09-19 07:08:08 +0000

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,66 @@
+import { useState, useEffect } from 'react'
+
+export default function Random() {
+  const [randomData, setRandomData] = useState([])
+  const [randomResult, setRandomResult] = useState([])
+
+  const randomUser = () => {
+    document.getElementById("result").innerHTML = null;
+    const data = document.getElementById("data").value;
+    setRandomData(data);
+    const aaaa = []
+    var randomLeft = data.split(/\n/);
+      for (let i = 0; i < (data.split(/\n/).length)/2; i++) {
+        const randoma = Math.floor((Math.random() * parseInt(randomLeft.length)) + 1)
+        aaaa.push(randomLeft[randoma -1])
+        randomLeft.splice(randoma -1,1)
+        const randomb = Math.floor((Math.random() * parseInt(randomLeft.length)) + 1)
+        aaaa.push(randomLeft[randomb -1])
+        console.log(randomLeft)
+        randomLeft.splice(randomb -1,1)
+      }
+      const bbbb = []
+      for (let x = 0; x < (aaaa.length/2); x++) {
+        bbbb.push({"a":aaaa[x+x],"b":aaaa[x+x+1]})
+      }
+      setRandomResult(bbbb)
+      const dom = document.getElementById("result")
+      for (let z = 0; z < (aaaa.length/2); z++) {
+        const newNode = document.createElement("p");
+        const a = typeof bbbb[z].a !== 'undefined' ? bbbb[z].a : ""
+        const b = typeof bbbb[z].b !== 'undefined' ? " - "+ bbbb[z].b : ""
+        const dt = z+1 + ") " + a + b
+        const textNode = document.createTextNode(dt);
+        newNode.appendChild(textNode);
+        dom.insertBefore(newNode, dom.children[z]);
+      }
+  }
+  const dataUpdate = () => {
+    const data = document.getElementById("data").value;
+    var pairs = (data.split(/\n/)).length / 2
+    document.getElementById("onChangePairs").innerHTML = Math.round(pairs);
+  }
+  return (
+    <div className="bg-gradient-to-tr from-green-300 to-blue-500 min-h-[100vh] overflow-y-auto max-h-auto w-screen flex flex-col justify-center items-center overflow-x-hidden">
+      <div className="backdrop-blur-sm bg-white/30 w-auto h-auto p-10 rounded-[10px]">
+        <div className="">
+          <a className="font-mono text-lg font-bold text-gray-700">Random Pair Generator</a>
+        </div>
+        <div className="bg-white w-[500px] h-auto">
+          <div className="bg-white w-[500px] h-[50px] drop-shadow-lg">
+            <div className="w-[50px] h-max p-2 float-left">
+              <p className="font-mono text-gray-700 text-sm">Pairs</p>
+              <p className="font-mono text-black text-lg font-bold leading-3" id="onChangePairs">0</p>
+            </div>
+            <button className="float-right w-[100px] h-[100%] bg-gray-200 font-bold text-gray-700 p-2 font-mono hover:bg-gray-300 duration-150" onClick={randomUser}>Random</button>
+          </div>
+          <textarea className="w-[500px] h-[200px] font-mono text-sm font-medium text-gray-700 p-2 outline-none" id="data" onChange={dataUpdate} />
+        </div>
+        <div>
+          <a className="font-mono text-sm text-gray-700 font-bold">Result:</a>
+        </div>
+        <div className="font-mono text-xs text-gray-700" id="result"></div>
+      </div>
+    </div>
+  )
+}
